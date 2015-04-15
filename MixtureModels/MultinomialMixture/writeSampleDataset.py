7487f206e8bba2de76e797eca1db1b2577651c6c
max <max@foursquare.com>
2015-04-15 17:31:45 -0400

    Refactoring Multinomial Mix Dir

--- MixtureModels/MultinomialMixture/writeSampleDataset.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import multinomialMixtureEstimation as MME
-
-model = MME.importFile("multinomialMixtureExample.txt")
-
-for i in range(0, 500):
-  sample = model.sampleRow(8)
-  print("\t".join(map(str, sample)))
